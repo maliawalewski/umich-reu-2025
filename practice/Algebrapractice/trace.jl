424c9cd8e0c60065250821c74aa9bf6352fba0a8
CalebBunch <rcbcg123@gmail.com>
2025-06-17 11:46:06 -0400

    changes to trace.jl

--- practice/Algebrapractice/trace.jl
+++ practice/Algebrapractice/trace.jl
@@ -3,6 +3,11 @@ using AbstractAlgebra
 
 R, (x, y) = GF(2^31-1)["x", "y"]
 
-trace, gb = groebner_learn([x*y^2 + x, y*x^2 + y])
+trace, gb = groebner_learn([x*y^2 + x, y*x^2 + y], ordering=DegRevLex())
+println(trace)
 
+trace, gb = groebner_learn([x*y^2 + x, y*x^2 + y], ordering=DegLex())
+println(trace)
+
+trace, gb = groebner_learn([x*y^2 + x, y*x^2 + y], ordering=Lex())
 println(trace)
