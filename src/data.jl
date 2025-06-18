90c9432e6c5da703ae31aaf952d852bed34a5d52
CalebBunch <rcbcg123@gmail.com>
2025-06-18 11:58:12 -0400

    fix data.jl

--- src/data.jl
+++ src/data.jl
@@ -6,39 +6,61 @@ function generate_ideal(;
     num_polynomials::Integer = 3,
     num_variables::Integer = 3,
     max_degree::Integer = 8,
-    max_terms::Integer = 6,
+    num_terms::Integer = 6,
+    max_attempts::Integer = 100,
 )
     @assert num_polynomials > 0 "num_polynomials must be greater than 0"
     @assert num_variables > 0 "num_variables must be greater than 0"
     @assert max_degree > 0 "max_degree must be greater than 0"
-    @assert max_terms > 0 "max_terms must be greater than 0"
+    @assert num_terms > 0 "num_terms must be greater than 0"
 
     field = GF(32003)
     ring, vars = polynomial_ring(field, ["x_" * string(i) for i = 1:num_variables])
 
     polynomials = []
-
+    used_polys = Set{String}()
     for _ = 1:num_polynomials
-        terms = []
-        num_terms = rand(1:max_terms)
-        for _ = 1:num_terms
-            coeff = rand(field)
-            exponents = rand(0:max_degree, num_variables)
-            monomial = coeff * prod(vars[i]^exponents[i] for i = 1:num_variables)
-            push!(terms, monomial)
+        p_attempts = 0 
+        while true
+            used_exponents = Set{NTuple{num_variables, Int}}()
+            terms = []
+            for _ = 1:num_terms
+                attempts = 0
+                while true
+                    exponents = rand(0:max_degree, num_variables)
+                    expt_key = Tuple(exponents)
+                    if !(expt_key in used_exponents)
+                        push!(used_exponents, expt_key)
+                        monomial = rand(field) * prod(vars[i]^exponents[i] for i in 1:num_variables)
+                        push!(terms, monomial)
+                        break
+                    end
+                    attempts += 1
+                    @assert attempts <= max_attempts "failed to generate a unique random monomial after $max_attempts attempts"
+                end
+            end
+            polynomial = sum(terms)
+            poly_str = string(polynomial)
+            if !(poly_str in used_polys)
+                push!(used_polys, poly_str)
+                push!(polynomials, polynomial)
+                break
+            end
+            p_attempts += 1
+            @assert p_attempts <= max_attempts "failed to generate a unique random polynomial after $max_attempts attempts"
         end
-        push!(polynomials, sum(terms))
     end
 
     return polynomials
 end
 
 function generate_data(;
-    num_ideals::Integer = 10000,
+    num_ideals::Integer = 1000,
     num_polynomials::Integer = 3,
     num_variables::Integer = 3,
     max_degree::Integer = 8,
-    max_terms::Integer = 6,
+    num_terms::Integer = 6,
+    max_attempts::Integer = 100,
 )
     @assert num_ideals > 0 "num_ideals must be greater than 0"
 
@@ -49,7 +71,8 @@ function generate_data(;
             num_polynomials = num_polynomials,
             num_variables = num_variables,
             max_degree = max_degree,
-            max_terms = max_terms,
+            num_terms = num_terms,
+            max_attempts = max_attempts,
         )
         push!(ideals, ideal)
     end
