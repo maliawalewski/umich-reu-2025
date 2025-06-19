0564ad2d6f19bb3b65ff5699b1384001c33685db
CalebBunch <rcbcg123@gmail.com>
2025-06-19 16:00:48 -0400

    change to main.jl

--- src/main.jl
+++ src/main.jl
@@ -3,30 +3,6 @@ include("environment.jl")
 include("data.jl")
 include("model.jl")
 
-function test_main()
-    num_vars = 3
-    delta_noise = 0.1f0
-    env = init_environment(numVars = num_vars, delta_noise = delta_noise)
-    # fill_ideal(env, 5, 3, 10, 100)
-
-    # Example action
-    raw_action = [1.0f0, 2.0f0, 3.0f0]
-    action = make_valid_action(raw_action, env)
-    println("Action: ", action)
-
-    if in_action_space(action, env)
-        basis = act!(env, action)
-        println("Basis: ")
-        println(basis)
-        println("is groebner basis? ")
-        println(isgroebner(basis))
-    else
-        println("Action is out of bounds.")
-    end
-
-    println("Current state: ", state(env))
-end
-
 function main()
     num_vars = 3
     delta_noise = 0.1f0
